a86248e5b15275c368f16f7d6d2f87f1cb6778e0
TalhahaRana <talhaha.rana1@gmail.com>
2024-10-09 17:15:12 +0500

    design the file structure

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <Navbar />
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/nav/Navbar.vue';
+import Footer from '@/components/nav/Footer.vue';
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    Footer
   },
 };
 </script>
