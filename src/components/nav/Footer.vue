525c4ac5e50241c5244aa0a1d3852100ab8f9f8f
Qasim Ikram <qasimlab118@gmail.com>
2024-10-10 18:04:05 +0500

    sd

--- src/components/nav/Footer.vue
+++ src/components/nav/Footer.vue
@@ -3,18 +3,13 @@
       <p>© 2024 Company Name</p>
     </footer>
   </template>
-  
+
   <script>
   export default {
     name: "Footer",
   };
   </script>
-  
+
   <style scoped>
-  footer {
-    position: absolute;
-    width: 100%;
-    bottom: 0;
-  }
+
   </style>
-  
