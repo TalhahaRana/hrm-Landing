4ce9b1e03a6493c17efcd02bea298365c5e0e60e
Mujahid Nawaz <mujahidnawaz145@gmail.com>
2024-10-11 15:54:59 +0500

    mujahid component

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/components/landingpage/home.vue'; // Ensure this path is correct
 import Blogs from '@/components/nav-component/resources/Blogs.vue'
+import AboutUs from '@/components/nav-component/company/AboutUs.vue'
+import NewsRoom from '@/components/nav-component/company/NewsRoom.vue'
 const routes = [
   { path: '/', name: 'Home', component: Home },
   {
     path:'/blogs', name:'Blogs', component: Blogs
+  },
+  {
+    path:'/about-us', name:'AboutUs', component: AboutUs
+  },
+  {
+    path:'/news-room', name:'NewsRoom', component: NewsRoom
   }
 
   
